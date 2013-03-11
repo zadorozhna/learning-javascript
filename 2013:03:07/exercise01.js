fdd2030cafe6e6d8bb05478e418a15f414f69c4e
zadorozhna <anastasiya.zadorozhna@gmail.com>
2013-03-11 17:22:21 +0100

    Update exercise01.js

--- 2013:03:07/exercise01.js
+++ 2013:03:07/exercise01.js
@@ -5,14 +5,10 @@ function identity (n) {
 	var i;
 	var j;
 
-	for(i=0; i<n; i++){
-		for(j=0; j<n; j++){
-			if(i === j){
-				document.write(' 1 ');
-			}
-			else{
-				document.write(' 0 ');
-			}
+	for (i=0; i<n; i++) {
+		for (j=0; j<n; j++) {
+			if (i === j) {document.write(' 1 ');}
+			else {document.write(' 0 ');}
 		}
 		document.write('<br>');
 	}
