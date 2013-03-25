5311810f0bdde54e55b6e07582b0d1531b9c531e
zadorozhna <anastasiya.zadorozhna@gmail.com>
2013-03-25 11:49:28 +0100

    Update exercise03.js

--- 2013:03:21/exercise03.js
+++ 2013:03:21/exercise03.js
@@ -6,7 +6,7 @@ function Point2D (x, y) {
 function Edge (from, to) {
 	this.from = from;
 	this.to = to;
-	this.length = function () {
+	Edge.prototype.length = function () {
 			      	return Math.sqrt(Math.pow((this.from.x - this.to.x), 2) + Math.pow((this.from.y - this.to.y), 2));
 				  }
 }
@@ -20,13 +20,13 @@ function Triangle (edge1, edge2, edge3) {
 
 	//exercise03b
 	//a method that compute the perimeter of the triangle
-	this.perimeter = function () {
+	Triangle.prototype.perimeter = function () {
 					 	return (this.edge1.length() + this.edge2.length() + this.edge3.length());
 					 }
 
 	//exercise03c
 	//a method that compute the area of the triangle
-	this.area = function () {
+	Triangle.prototype.area = function () {
 					var s = (this.perimeter() / 2);
 					return Math.sqrt(s * (s - this.edge1.length()) * (s - edge2.length()) * (s - edge3.length()));
 				}
